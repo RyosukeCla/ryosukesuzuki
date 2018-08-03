54a3b4b33e0bd20977ff0cd01d16dec9bcab4380
Ryosuke Suzuki <ryosukeclarinet@gmail.com>
2018-08-03 18:57:55 +0900

    commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const minimist = require('minimist')
+const chalk = require('chalk')
+const ascii = require('ascii-art')
+const path = require('path')
+const argv = minimist(process.argv.splice(2))
+
+var asciify = require('asciify-image');
+
+var options = {
+  fit:    'box',
+  width:  13,
+  height: 13
+}
+
+asciify(path.resolve(__dirname, './icon.PNG'), options, function (err, asciified) {
+  if (err) throw err;
+  const split = asciified.split('\n')
+  const middle = parseInt(split.length / 2)
+  split[middle - 4] += `        Hi! I'm ${chalk.bold.italic('Ryosuke Suzuki')}`
+  split[middle - 2] += '        ## me'
+  split[middle - 1] += '        - software developer / student'
+  split[middle - 0] += '        - nodejs / typescript / vue / docker'
+  split[middle + 2] += '        ## sns'
+  split[middle + 3] += chalk.blue('        - twitter  : @GentleClarinet')
+  split[middle + 4] += chalk.green('        - github   : @RyosukeCla')
+  console.log(split.join('\n'))
+  // console.log(asciified);
+});
+
+if (argv.v || argv.version) {
+
+}
